6b9ade56e4222ab7e4c3d923c7e24a9a17c1abf2
kisphapha <thanhhung16082003@gmail.com>
2024-07-04 23:56:09 +0700

    basic controller and collision

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    [SerializeField] private float turningSpeed = 90f;
+    private ParticleSystem dustParticles;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        dustParticles = transform.Find("Dust Particles").GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // Rotate object to the left
+            rb.AddTorque(1);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // Rotate object to the right
+            rb.AddTorque(-1);
+
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            dustParticles.Play();
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            dustParticles.Stop();
+        }
     }
 }
