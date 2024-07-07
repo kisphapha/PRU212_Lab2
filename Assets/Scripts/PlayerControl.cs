0077519c6403507b466898b5ea47ba5ef041118a
kisphapha <thanhhung16082003@gmail.com>
2024-07-07 23:19:31 +0700

    bonus coin and more controll

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,12 +7,18 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private float turningSpeed = 90f;
     private ParticleSystem dustParticles;
     private Rigidbody2D rb;
+    private GameObject scoreTextGO;
     public float torque = 4;
+    public float jumpForce = 10f;
+    public float speedMultiplier = 1.05f;
+    public float slowMultiplier = 0.95f;
+    private bool isGrounded = false;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         dustParticles = transform.Find("Dust Particles").GetComponent<ParticleSystem>();
+        scoreTextGO = GameObject.FindGameObjectWithTag("ScoreTextTag");
     }
 
     // Update is called once per frame
@@ -29,20 +35,45 @@ public class PlayerControl : MonoBehaviour
             rb.AddTorque(-torque);
 
         }
+        if (Input.GetKey(KeyCode.UpArrow) && isGrounded)
+        {
+            rb.velocity *= speedMultiplier;
+        }
+        // Slow down
+        if (Input.GetKey(KeyCode.DownArrow) && isGrounded)
+        {
+            rb.velocity *= slowMultiplier;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Debug.Log("Jumping");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Ground"))
         {
+            isGrounded = true;
             dustParticles.Play();
         }
+        
     }
     void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Ground"))
         {
+            isGrounded = false;
             dustParticles.Stop();
         }
     }
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("BonusItem"))
+        {
+            scoreTextGO.GetComponent<GameScore>().Score += 20;
+            Destroy(collision.gameObject);
+        }
+    }
 }
